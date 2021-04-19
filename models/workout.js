ef9d80188ba76c4b687d14f1eca84aeadd161d57
Omar Dahmash <omartdh@omars-mbp.home>
2021-04-19 14:55:46 -0700

    added server.js file and models

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter a name for exercise"
+    },
+    duration: {
+      type: Number,
+      required: "Enter duration"
+    },
+    weight: {
+      type: Number,
+      required: "Enter weight"
+    },
+    reps: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    }
+  },  
+});
+
+const Workout = mongoose.model("Exercise", workoutSchema);
+
+module.exports = Workout;
+
